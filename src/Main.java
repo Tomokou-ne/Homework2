10a414efef0fdd66b6cf624282abd52b4befe952
Tomokou <bychckova.margo@yandex.ru>
2022-11-14 16:34:31 +0300

    Сделано дз Строки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        //Задание 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+        //Задание 2
+        String fullNameUp = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameUp);
+        //Задание 3
+        String fullNameOne = "Иванов Семён Семёнович";
+        String fullNameFix = fullNameOne.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника — " + fullNameFix);
+            }
+
+        }
+
+
+
