a1fa514f071d42bc2cbe502eb45bb505979d00da
Tomokou <bychckova.margo@yandex.ru>
2022-11-19 15:48:32 +0300

    Сделано дз Методы

--- src/Main.java
+++ src/Main.java
@@ -38,11 +38,11 @@ public class Main {
     public static String calculateTime(int deliveryDistance) {
         int deliveryTime = 0;
         if (deliveryDistance < 20) {
-            deliveryTime = deliveryTime +1;
+            deliveryTime = deliveryTime + 1;
         } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
-            deliveryTime = deliveryTime +2;
+            deliveryTime = deliveryTime + 2;
         } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
-            deliveryTime = deliveryTime +3;
+            deliveryTime = deliveryTime + 3;
         }return "Потребуется дней: " + deliveryTime;
     }
 
